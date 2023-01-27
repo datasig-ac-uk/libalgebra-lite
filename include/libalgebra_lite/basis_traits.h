88222828a1101591756727d5e8cf2cf2f94d58cc
inakleinbottle <admin@inakleinbottle.com>
2023-01-27 09:17:15 +0000

    Fixed bad names in trait

--- include/libalgebra_lite/basis_traits.h
+++ include/libalgebra_lite/basis_traits.h
@@ -7,6 +7,7 @@
 
 #include "implementation_types.h"
 
+#include <algorithm>
 #include <functional>
 #include <utility>
 #include <memory>
@@ -50,7 +51,7 @@ struct basis_trait {
     static deg_t degree(const Basis& basis, const key_type& key) noexcept
     { return basis.degree(key); }
     static deg_t max_degree(const Basis& basis) noexcept
-    { return basis.max_degree(); }
+    { return basis.depth(); }
 
     static dimn_t start_of_degree(const Basis& basis, deg_t deg) noexcept
     { return basis.start_of_degree(deg); }
@@ -58,9 +59,14 @@ struct basis_trait {
     { return basis.size(static_cast<int>(deg)); }
     static std::pair<dimn_t, deg_t> get_next_dimension(const Basis& basis, dimn_t dim, deg_t hint=0)
     {
-        auto key = index_to_key(basis, dim);
-        auto deg = degree(basis, key);
-        return {size(basis, deg), deg};
+        const auto& sizes = basis.sizes();
+        const auto begin = sizes.begin();
+        const auto end = sizes.end();
+        auto it = std::lower_bound(begin, end, dim, std::less_equal<>());
+        if (it == end) {
+            return {max_dimension(basis), 0};
+        }
+        return {*it, static_cast<deg_t>(it - begin)};
     }
 
     using key_ordering = std::less<key_type>;
