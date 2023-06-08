01d10566c495b239fdd6032e76bf4d31ce772cc0
Sam Morley <sam@inakleinbottle.com>
2023-06-08 17:35:20 +0100

    move accessing methods to non-inline

--- include/libalgebra_lite/registry.h
+++ include/libalgebra_lite/registry.h
@@ -15,18 +15,16 @@
 
 #include <boost/functional/hash.hpp>
 
-
 #include "basis.h"
 
 namespace lal {
 
 template <typename Basis>
-class basis_registry
-{
+class basis_registry {
     using basis_pointer = lal::basis_pointer<Basis>;
     static std::mutex m_lock;
     static std::unordered_map<std::pair<deg_t, deg_t>,
-        std::unique_ptr<const Basis>, boost::hash<std::pair<deg_t, deg_t>>> m_cache;
+                              std::unique_ptr<const Basis>, boost::hash<std::pair<deg_t, deg_t>>> m_cache;
 
 public:
 
@@ -34,17 +32,7 @@ public:
      * Most of the basis types require a width and depth pair
      */
 
-    static basis_pointer get(deg_t width, deg_t depth)
-    {
-        std::lock_guard<std::mutex> access(m_lock);
-
-        auto& found = m_cache[{width, depth}];
-        if (!found) {
-            found = std::make_unique<const Basis>(width, depth);
-        }
-
-        return basis_pointer(found);
-    }
+    static basis_pointer get(deg_t width, deg_t depth);
 
 };
 
@@ -53,13 +41,11 @@ std::mutex basis_registry<Basis>::m_lock;
 
 template <typename Basis>
 std::unordered_map<std::pair<deg_t, deg_t>, std::unique_ptr<const Basis>,
-        boost::hash<std::pair<deg_t, deg_t>>>
+                   boost::hash<std::pair<deg_t, deg_t>>>
     basis_registry<Basis>::m_cache;
 
-
 template <typename Multiplication>
-class multiplication_registry
-{
+class multiplication_registry {
     static std::mutex m_lock;
     static std::unordered_map<deg_t, std::shared_ptr<const Multiplication>> m_cache;
 
@@ -70,27 +56,14 @@ public:
      * parameter, which is really only there to help validate the
      * bases of the vector arguments.
      */
-    static std::shared_ptr<const Multiplication> get(deg_t width)
-    {
-        std::lock_guard<std::mutex> access(m_lock);
-
-        auto& found = m_cache[width];
-        if (found) {
-            return found;
-        }
-
-        return found = std::make_shared<const Multiplication>(width);
-    }
+    static std::shared_ptr<const Multiplication> get(deg_t width);
 
     template <typename Basis>
-    static std::shared_ptr<const Multiplication> get(const Basis& basis)
-    {
+    static std::shared_ptr<const Multiplication> get(const Basis &basis) {
         return get(basis.width());
     }
 
-
 };
-
 
 template <typename Multiplication>
 std::mutex multiplication_registry<Multiplication>::m_lock;
@@ -99,13 +72,33 @@ template <typename Multiplication>
 std::unordered_map<deg_t, std::shared_ptr<const Multiplication>>
     multiplication_registry<Multiplication>::m_cache;
 
+template <typename Basis>
+typename basis_registry<Basis>::basis_pointer basis_registry<Basis>::get(deg_t width, deg_t depth) {
 
+    std::lock_guard<std::mutex> access(m_lock);
+
+    auto &found = m_cache[{width, depth}];
+    if (!found) {
+        found = std::make_unique<const Basis>(width, depth);
+    }
+
+    return basis_pointer(found);
+}
+
+template <typename Multiplication>
+std::shared_ptr<const Multiplication> multiplication_registry<Multiplication>::get(deg_t width) {
+
+    std::lock_guard<std::mutex> access(m_lock);
+
+    auto &found = m_cache[width];
+    if (found) {
+        return found;
+    }
+
+    return found = std::make_shared<const Multiplication>(width);
 
 }
 
-
-
-
-
+}
 
 #endif //LIBALGEBRA_LITE_REGISTRY_H
