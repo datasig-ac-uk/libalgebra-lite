d83e60d4d41802feb4be876d7b5ffe8ced13f85f
inakleinbottle <admin@inakleinbottle.com>
2023-07-10 17:47:51 +0100

    Fixed bad inequalities

--- src/algebra/half_shuffle_multiplier.cpp
+++ src/algebra/half_shuffle_multiplier.cpp
@@ -85,7 +85,7 @@ left_half_shuffle_tensor_multiplier::operator()(
 {
     static const boost::container::small_vector<typename base_type::pair_type, 0> null;
 
-    if (static_cast<deg_t>(lhs.degree() + rhs.degree()) >= basis.depth()) {
+    if (static_cast<deg_t>(lhs.degree() + rhs.degree()) > basis.depth()) {
         return null;
     }
 
@@ -175,7 +175,7 @@ right_half_shuffle_tensor_multiplier::operator()(
 {
     static const boost::container::small_vector<typename base_type::pair_type, 0> null;
 
-    if (lhs.degree() + rhs.degree() >= static_cast<dimn_t>(basis.depth())) {
+    if (lhs.degree() + rhs.degree() > static_cast<dimn_t>(basis.depth())) {
         return null;
     }
 
