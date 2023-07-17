a91374f7b89ae26ff6f40dc10d701e0637c29ce1
Sam Morley <admin@inakleinbottle.com>
2023-07-17 13:39:48 +0100

    Lie multiplication free function

--- include/libalgebra_lite/lie.h
+++ include/libalgebra_lite/lie.h
@@ -11,8 +11,8 @@
 #include <mutex>
 #include <unordered_map>
 
-#include <boost/functional/hash.hpp>
 #include <boost/container/small_vector.hpp>
+#include <boost/functional/hash.hpp>
 
 #include "algebra.h"
 #include "hall_set.h"
@@ -24,7 +24,8 @@ class lie_multiplier;
 
 LAL_EXPORT_TEMPLATE_CLASS(base_multiplier, lie_multiplier, hall_basis, 2)
 
-class LIBALGEBRA_LITE_EXPORT lie_multiplier : public base_multiplier<lie_multiplier, hall_basis, 2>
+class LIBALGEBRA_LITE_EXPORT lie_multiplier
+    : public base_multiplier<lie_multiplier, hall_basis, 2>
 {
     using base_type = base_multiplier<lie_multiplier, hall_basis, 2>;
 
@@ -36,42 +37,45 @@ class LIBALGEBRA_LITE_EXPORT lie_multiplier : public base_multiplier<lie_multipl
 
     using parent_type = std::pair<key_type, key_type>;
 
-    mutable std::unordered_map<parent_type, product_type, boost::hash<parent_type>> m_cache;
+    mutable std::unordered_map<
+            parent_type, product_type, boost::hash<parent_type>>
+            m_cache;
     mutable std::recursive_mutex m_lock;
 
-    product_type key_prod_impl(const hall_basis& basis, key_type lhs, key_type rhs) const;
+    product_type
+    key_prod_impl(const hall_basis& basis, key_type lhs, key_type rhs) const;
 
 public:
     using basis_type = hall_basis;
 
-    explicit lie_multiplier(deg_t width) : m_width(width)
-    {}
+    explicit lie_multiplier(deg_t width) : m_width(width) {}
 
-    reference operator()(const hall_basis& basis, key_type lhs, key_type rhs) const;
-
-
+    reference
+    operator()(const hall_basis& basis, key_type lhs, key_type rhs) const;
 };
 
-
-struct LIBALGEBRA_LITE_EXPORT lie_multiplication : public base_multiplication<lie_multiplier>
-{
+struct LIBALGEBRA_LITE_EXPORT lie_multiplication
+    : public base_multiplication<lie_multiplier> {
     using base = base_multiplication<lie_multiplier>;
     using base::base;
 };
 
-
-
-template<typename Coefficients,
-        template<typename, typename> class VectorType,
-        template<typename> class StorageModel>
-using lie = algebra<hall_basis,
-                    Coefficients,
-                    lie_multiplication,
-                    VectorType,
-                    StorageModel>;
+template <
+        typename Coefficients, template <typename, typename> class VectorType,
+        template <typename> class StorageModel>
+using lie = algebra<
+        hall_basis, Coefficients, lie_multiplication, VectorType, StorageModel>;
 
 LAL_EXPORT_TEMPLATE_CLASS(multiplication_registry, lie_multiplication)
 
-} // namespace lal
+template <typename LLie, typename RLie>
+inline LLie lie_multiply(const LLie& left, const RLie& right)
+{
+    const auto liem
+            = multiplication_registry<lie_multiplication>::get(left.basis());
+    return multiply(*liem, left, right);
+}
 
-#endif //LIBALGEBRA_LITE_LIE_H
+}// namespace lal
+
+#endif// LIBALGEBRA_LITE_LIE_H
